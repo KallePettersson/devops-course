b4d7b224871a28b86b29c5813cfc89cd045bc5dd
andnil5 <andnil5@kth.se>
2021-05-27 11:53:57 +0200

    update

--- contributions/course-automation/kalpet-andnil5/dist/index.js
+++ contributions/course-automation/kalpet-andnil5/dist/index.js
@@ -5888,7 +5888,6 @@ module.exports = {
                 ? context.repo.repo : undefined,
     };
     if (!data.base || !data.head || !data.owner || !data.repo) {
-      console.log(data);
       throw Error(`Head, base, owner or repo are missing from the payload for this `+
       `${context.eventName} event. Please submit an issue on this action's GitHub repo.`
       );
@@ -6116,7 +6115,6 @@ const Parser = __nccwpck_require__(332);
 try {
   console.log('Retreving valid kthIDs');
   const kthIDs = Parser.parseKTHIdsFile();
-  console.log('idsss..' + kthIDs);
   console.log('Parsing PR payload and repo data.');
   const contextData = Parser.parseContext(context);
 
